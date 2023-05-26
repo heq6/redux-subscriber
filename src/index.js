b1da548ed4d8146819a0c674365a3ad5c7021e11
heq6 <heq6@xiaopeng.com>
2023-05-26 09:33:57 +0800

    perf: conpatible with multiple keys

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import {get} from 'object-path';
 
 const subscribers = {};
+let delimiter = '!';
 
-export function subscribe(key, cb) {
+export function subscribe(keys, cb) {
+  const key = Array.isArray(keys) ? keys.join(delimiter) : keys;
   if (subscribers.hasOwnProperty(key)) {
     subscribers[key].push(cb);
   } else {
@@ -10,21 +12,37 @@ export function subscribe(key, cb) {
   }
 
   // return "unsubscribe" function
-  return function sub() {
+  return function nm() {
     subscribers[key] = subscribers[key].filter(s => s !== cb);
   };
 }
+export default function(store, defaultDelimiter = delimiter) {
+  delimiter = defaultDelimiter;
+  let prevState = store.getState();
 
-export default function(store) {
   store.subscribe(() => {
     const newState = store.getState();
 
-    Object.keys(subscribers).forEach(key => {
-      const state = get(newState, key);
-      if (state) {
-        subscribers[key].forEach(cb => cb(state));
+    Object.keys(subscribers).forEach(subscriberkey => {
+      const keys = subscriberkey.split(delimiter);
+      const result = [];
+      let isAnyStateChanged = false;
+      keys.forEach(key => {
+        const keyState = get(newState, key);
+        result.push(keyState);
+        if (!isAnyStateChanged && get(prevState, key) !== keyState) {
+          isAnyStateChanged = true;
+        }
+      });
+      if (isAnyStateChanged) {
+        if (keys.length === 1) {
+          subscribers[subscriberkey].forEach(cb => cb(result[0]));
+        } else {
+          subscribers[subscriberkey].forEach(cb => cb(result));
+        }
       }
     });
+    prevState = newState;
   });
 
   return subscribe;
